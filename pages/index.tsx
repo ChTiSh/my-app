4d07a2b8d9d85b9c32aa529239031034c05df49c
ChTiSh <chentina.shen@gmail.com>
2024-03-09 22:38:46 -0800

    first change

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,17 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import { use, useEffect } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  useEffect(() => {
+    fetch("https://my-server-1-jttg.onrender.com/data")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  }, []);
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
